d5e55fb0631c7cbabc98a8519768950a0049e27d
Philipp Jung <philippjung@posteo.de>
2019-06-20 18:48:08 +0200

    Fix bug, Clean up notebook, expand test-coverage
    Wrote new unit tests, now almost every function in lib/ with the
    exception of run_ml_imputater_on_fd_set() is tested.
    
    Bugs are fixed. Every split dataset now has its index set as 0th column.
    All tests functions and tests expect this.
    
    Logic has been removed from fd_imputer_analysis.ipynb and has been
    stored into lib/fd_imputer.py as run_ml_imputer_on_fd_set() and
    run_fd_imputer_on_fd_set(). The notebook still needs to be updated.

--- lib/fd_imputer.py
+++ lib/fd_imputer.py
@@ -1,3 +1,19 @@
+def load_dataframes(splits_path, data_title, missing_value_token):
+    import pandas as pd
+    import numpy as np
+    df_train = pd.read_csv(splits_path+'train/' +
+                           data_title+'_train.csv', header=None)
+    df_validate = pd.read_csv(
+        splits_path+'validate/'+data_title+'_validate.csv', header=None)
+    df_test = pd.read_csv(splits_path+'test/' +
+                          data_title+'_test.csv', header=None)
+
+    dfs = [df_train, df_validate, df_test]
+    dfs = [df.replace(missing_value_token, np.nan) for df in dfs]
+
+    return (dfs)
+
+
 def read_fds(fd_path):
     """  Returns a dictionary with FDs
 
@@ -32,17 +48,17 @@ def read_fds(fd_path):
     return fd_dict
 
 
-def fd_imputer(df_test, df_train, fd):
+def fd_imputer(df_train, df_validate, fd):
     """ Imputes a column of a dataframe using a FD.
     Returns a dataframe with an additional column named
     rhs+'_imputed'. Only returns rows for which an imputation has been
     successfully performed. The returned dataframe's index is the one
-    used by df_test.
+    used by df_validate.
 
     Keyword arguments:
-    df_test -- dataframe where a column shall be imputed
-    df_train -- dataframe on which fds were detected
-    fd -- dictionary containing the RHS as key and a list of LHS as value
+        df_train -- dataframe on which fds were detected
+        df_validate -- dataframe on which a column shall be imputed
+        fd -- dictionary containing the RHS as key and a list of LHS as value
     """
     import pandas as pd
     rhs = list(fd)[0]  # select the fd right hand side
@@ -60,17 +76,93 @@ def fd_imputer(df_test, df_train, fd):
     """ reset_index() and set_index() is a hacky way to save df_test's index
     which is normally lost due to pd.merge(). """
     df_imputed = pd.merge(df_train_reduced,
-                          df_test.iloc[:, relevant_cols].reset_index(),
+                          df_validate.iloc[:, relevant_cols].reset_index(),
                           on=lhs,
                           suffixes=('_imputed', '')).set_index('index')
 
-    df_test_imputed = pd.merge(df_test,
-                               df_imputed.loc[:, str(rhs)+'_imputed'],
-                               left_index=True,
-                               right_index=True,
-                               how='left')
+    df_validate_imputed = pd.merge(df_validate,
+                                   df_imputed.loc[:, str(rhs)+'_imputed'],
+                                   left_index=True,
+                                   right_index=True,
+                                   how='left')
 
-    return df_test_imputed
+    return df_validate_imputed
+
+
+def run_fd_imputer_on_fd_set(df_train, df_validate, fds, continuous_cols=[]):
+    """ Runs fd_imputer for every fd contained in a dictionary on a split df.
+
+    Executes fd_imputer() for every fd in fds. df_train and df_split should be
+    created using split_df(). continuous_cols is used to distinguish between
+    columns containing continuous_cols numbers and classifiable objects.
+
+    Keyword arguments:
+        df_train -- a df on which fds have been detected. Needs to have the
+        same origin df as df_validate
+        df_validate -- a df on which no fds have been detected. Needs to have
+        the same origin df as df_train
+        fds -- a dictionary with an fd's possible rhs as values and a list of
+        lhs-combinations as value of fds[rhs].
+
+    Returns:
+    A dictionary consisting of rhs's as keys with associated performance
+    metrics for each lhs. Performance metrics are precision, recall and
+    f1-measure for classifiable data and the mean squared error as well as
+    the number of unsucessful imputations for continuous data.
+    """
+    from sklearn import metrics
+    fd_imputer_results = {}
+
+    for rhs in fds:
+        rhs_results = []
+
+        for lhs in fds[rhs]:
+            fd = {rhs: lhs}
+            print(fd)
+
+            df_fd_imputed = fd_imputer(df_train, df_validate, fd)
+
+            # make sure that value for missing data is of same type as
+            # row to be imputed to avoid mix of labels and continuous numbers
+            if isinstance(df_fd_imputed.iloc[0, rhs], str):
+                df_fd_imputed = df_fd_imputed.fillna('no value')
+                y_pred = df_fd_imputed.loc[:, str(rhs)+'_imputed']
+                y_true = df_fd_imputed.loc[:, rhs]
+            else:
+                df_imputed_col = df_fd_imputed.loc[:, str(rhs)+'_imputed']
+                nan_result_selector = df_imputed_col.isna()
+                nans = nan_result_selector.sum()
+
+                # ignore nans to compute MSE
+                y_pred = df_fd_imputed.loc[~nan_result_selector,
+                                           str(rhs)+'_imputed']
+                y_true = df_fd_imputed.loc[~nan_result_selector,
+                                           rhs]
+
+            if rhs in continuous_cols:
+                mse = ''
+
+                if len(y_pred) > 0:
+                    mse = metrics.mean_squared_error(y_true, y_pred)
+
+                result = {'nans': nans, 'lhs': lhs, 'mse': mse}
+            else:
+                result = {
+                    'lhs': lhs,
+                    'f1': metrics.f1_score(y_true,
+                                           y_pred,
+                                           average='weighted'),
+                    'recall': metrics.recall_score(y_true,
+                                                   y_pred,
+                                                   average='weighted'),
+                    'precision': metrics.precision_score(y_true,
+                                                         y_pred,
+                                                         average='weighted')
+                }
+            rhs_results.append(result)
+        fd_imputer_results[rhs] = rhs_results
+
+    return fd_imputer_results
 
 
 def ml_imputer(df_train, df_validate, df_test, impute_column):
@@ -108,10 +200,75 @@ def ml_imputer(df_train, df_validate, df_test, impute_column):
     return predictions
 
 
-def df_split(data_title, df, split_ratio, splits_path=''):
+def run_ml_imputer_on_fd_set(df_train, df_validate, df_test, fds,
+                             continuous_cols=[]):
+    """ This needs documentation
+    """
+    from sklearn import metrics
+    ml_imputer_results = {}
+    for rhs in fds:
+        rhs_results = []
+        for lhs in fds[rhs]:
+            relevant_cols = lhs + [rhs]
+
+            # make sure that datawig doesn't perform regression on categories
+            # also, select relevant subsets
+            if rhs not in continuous_cols:
+                df_subset_train = df_train.iloc[:, relevant_cols].astype(
+                    {rhs: str})
+                df_subset_validate = df_validate.iloc[:, relevant_cols].astype(
+                    {rhs: str})
+                df_subset_test = df_test.iloc[:, relevant_cols].astype(
+                    {rhs: str})
+            else:
+                df_subset_train = df_train.iloc[:, relevant_cols]
+                df_subset_validate = df_validate.iloc[:, relevant_cols]
+                df_subset_test = df_test.iloc[:, relevant_cols]
+
+            print(lhs, rhs)
+            df_imputed = fd_imputer.ml_imputer(df_subset_train,
+                                               df_subset_validate,
+                                               df_subset_test,
+                                               str(rhs))
+
+            y_pred = df_imputed.loc[:, str(rhs)+'_imputed']
+            y_true = df_imputed.loc[:, str(rhs)]
+
+            if rhs in continuous_cols:
+                result = {
+                    'lhs': lhs,
+                    'mse': metrics.mean_squared_error(y_true, y_pred)
+                }
+            else:
+                result = {
+                    'lhs': lhs,
+                    'f1': metrics.f1_score(y_true,
+                                           y_pred,
+                                           average='weighted'),
+                    'recall': metrics.recall_score(y_true,
+                                                   y_pred,
+                                                   average='weighted'),
+                    'precision': metrics.precision_score(y_true,
+                                                         y_pred,
+                                                         average='weighted')
+                }
+            rhs_results.append(result)
+        ml_imputer_results[rhs] = rhs_results
+    return ml_imputer_results
+
+
+def index_as_first_column(df):
+    df = df.reset_index()
+    df.columns = [x for x in range(0, len(df.columns))]
+    return df
+
+
+def split_df(data_title, df, split_ratio, splits_path=''):
     """ Splits a dataframe into train-, validate- and test-subsets.
     If a splits_path is provided, splits will be saved to the harddrive.
-    Returns a tuple (df_train, df_validate, df_test).
+
+    Returns a tuple (df_train, df_validate, df_test) if the splits_path
+    is an empty string.
 
     Keyword arguments:
     data_title -- a string naming the data
@@ -121,8 +278,15 @@ def df_split(data_title, df, split_ratio, splits_path=''):
     """
     import os
     from datawig.utils import random_split
+    import numpy as np
 
     ratio_train, ratio_validate, ratio_test = (split_ratio)
+
+    # compare first col with index. if not equal...
+    if not np.array_equal(df.iloc[:, 0].values, np.array(df.index)):
+        # ...set index as 0th column such that fd_imputer can use it to impute
+        print('kein doppelter index')
+        df = index_as_first_column(df)
 
     splits = {
         'train': {'path': splits_path+'train/',
--- lib/test_fd_imputer.py
+++ lib/test_fd_imputer.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+import numpy as np
 import fd_imputer
 import tempfile
 
@@ -7,16 +8,67 @@ import tempfile
 class TestFdImputer(unittest.TestCase):
 
     def setUp(self):
-        # make up test data
-        self.df_test = pd.DataFrame([[1, 2, 3], [4, 5, 9], [5, 7, 12]],
-                                    columns=[0, 1, 2])
-        self.df_train = pd.DataFrame([[1, 2, 4], [7, 8, 15], [11, 13, 24]],
+        # set up test data
+        self.data_title = 'test'
+        self.continuous_cols = [0, 1, 2, 3]
+        self.fd = {3: [2, 1]}
+        self.df_validate = pd.DataFrame([[1, 2, 3], [4, 5, 9], [5, 7, 12]],
+                                        columns=[0, 1, 2])
+        self.df_train = pd.DataFrame([[1, 2, 4], [7, 8, 15], [11, 13, 24],
+                                      [np.nan, np.nan, np.nan]],
                                      columns=[0, 1, 2])
-        self.fd = {2: [1, 0]}
+        self.df_test = pd.DataFrame([[8, 9, 17], [10, 10, 20], [20, 30, 50]],
+                                    columns=[0, 1, 2])
+        self.test_df = pd.concat([self.df_train,
+                                  self.df_validate,
+                                  self.df_test],
+                                 ignore_index=True)
 
-        self.split_df = pd.DataFrame([[1, 2, 3], [2, 1, 3], [5, 6, 7],
-                                      [3, 1, 5], [4, 3, 1], [4, 1, 7]])
+        # real data from production for testing
         self.fds = fd_imputer.read_fds('test_data/test_fd.txt')
+
+    def test_run_fd_imputer_on_fd_set(self):
+        df_train, df_validate = (map(fd_imputer.index_as_first_column,
+                                     [self.df_train,
+                                      self.df_validate]))
+        fd = self.fd
+        fd[3] = [fd[3]]
+        result = fd_imputer.run_fd_imputer_on_fd_set(df_train,
+                                                     df_validate,
+                                                     self.fd,
+                                                     self.continuous_cols)
+
+        self.assertEqual(len(result.keys()), 1)
+        self.assertEqual(list(result.keys())[0], 3)
+        self.assertEqual(2, result[3][0]['nans'])
+
+    def test_ml_imputer(self):
+        df_train, df_validate, df_test = fd_imputer.split_df(
+            'adult', self.test_df, [0.6, 0.2, 0.2])
+        df_imputed = fd_imputer.ml_imputer(df_train,
+                                           df_validate,
+                                           df_test,
+                                           3)
+
+        self.assertTrue(df_imputed.shape[1], self.test_df.shape[1]+1)
+        self.assertTrue(df_test.shape[0], df_imputed.shape[0])
+
+    def test_split_df(self):
+        # does split_df() set the index as the 0th column?
+        df_train, df_validate, df_test = fd_imputer.split_df(
+            'adult', self.test_df, [0.6, 0.2, 0.2])
+
+        train_index = np.array(df_train.index)
+        train_index_col = np.array(df_train.iloc[:, 0].values)
+        self.assertTrue(np.array_equal(train_index, train_index_col))
+
+        # does split_df() recognize if the index is already a column?
+        df_train, df_validate, df_test = fd_imputer.split_df(
+            'adult', self.test_df.reset_index(), [0.6, 0.2, 0.2])
+
+        no_train_cols = len(df_train.columns)
+        no_original_cols = len(self.test_df.reset_index().columns)
+        self.assertTrue(no_train_cols == no_original_cols)
 
     def test_read_fds(self):
         rhs = [x for x in self.fds]
@@ -27,30 +79,34 @@ class TestFdImputer(unittest.TestCase):
         self.assertTrue(min(lhs_flat) == 0)
         self.assertTrue(max(lhs_flat) == (len(rhs)))
 
-    def test_df_split(self):
+    def test_split_load_df(self):
         with tempfile.TemporaryDirectory() as tempdirname:
-            fd_imputer.df_split(
-                'test', self.split_df, [0.33, 0.33, 0.33], tempdirname+'/')
+            fd_imputer.split_df(
+                self.data_title, self.test_df, [0.33, 0.33, 0.33],
+                tempdirname+'/')
 
-            df_train = pd.read_csv(
-                tempdirname+'/train/test_train.csv', header=None)
-            df_validate = pd.read_csv(
-                tempdirname+'/validate/test_validate.csv', header=None)
-            df_test = pd.read_csv(
-                tempdirname+'/test/test_test.csv', header=None)
+            df_train, df_validate, df_test = fd_imputer.load_dataframes(
+                tempdirname+'/', self.data_title, 'noData')
 
-            no_duplicates = sum(
-                pd.concat([df_test, df_train, df_validate]).duplicated())
-            self.assertEqual(no_duplicates, 0)
+            df_glued = pd.concat([df_train, df_validate, df_test])
+            self.assertEqual(sum(df_glued.duplicated()), 0)
+
+            # check if missing value replacement from load_dataframes works
+            self.assertTrue(df_glued.isna().values.sum() == 3)
+
+            self.assertTrue(df_glued.shape[0] <= self.test_df.shape[0])
 
     def test_fd_imputer(self):
+        df_train, df_validate = (map(fd_imputer.index_as_first_column,
+                                     [self.df_train,
+                                      self.df_validate]))
         self.assertEqual(fd_imputer.fd_imputer(
-            self.df_test, self.df_train, self.fd).index[0],
+            df_train, df_validate, self.fd).index[0],
             0)
         self.assertEqual(fd_imputer.fd_imputer(
-            self.df_test,
-            self.df_train,
-            self.fd).loc[:, '2_imputed'].dropna()[0],
+            df_train,
+            df_validate,
+            self.fd).loc[:, '3_imputed'].dropna()[0],
             4)
 
 
